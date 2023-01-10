434cf32d08efb72afffbc297d69ba754a0a95b84
nanase05251994 <117430938+nanase05251994@users.noreply.github.com>
2023-01-10 18:53:31 +0800

    Update settings.py

--- SatsukiFC/settings.py
+++ SatsukiFC/settings.py
@@ -114,11 +114,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
@@ -133,3 +128,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# The URL to use when referring to static files (where they will be served from)
+STATIC_URL = '/static/'
